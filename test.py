e85f0d2d0c2c4e724c8843996aef293dc7e2acc6
illuminati <pmudavath@agility.com>
2020-08-20 13:43:45 +0400

    version2 changes

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 data = pd.read_csv("data/data.csv")
 
+print(data.head())
+
+data.drop(["quote_id", "quote_num"], inplace=True, axis=1)
+
+data.to_csv("data/data.csv")
 
 print(data.head())
